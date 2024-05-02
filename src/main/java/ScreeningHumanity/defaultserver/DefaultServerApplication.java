9c886daac58b4d8a1c1611e034b958140f0f394b
KangBaekGwa <ksu9801@gmail.com>
2024-05-02 10:56:26 +0900

    feat : swagger 관련 설정 추가

--- src/main/java/ScreeningHumanity/defaultserver/DefaultServerApplication.java
+++ src/main/java/ScreeningHumanity/defaultserver/DefaultServerApplication.java
@@ -1,13 +1,24 @@
 package ScreeningHumanity.defaultserver;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+//@OpenAPIDefinition(
+//        servers = {
+//                @Server(url = "http://localhost:8282", description = "Local Test Server URL"),
+//                @Server(url = "https://sssg.shop", description = "Server URL")
+//        }
+//)
 public class DefaultServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DefaultServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultServerApplication.class, args);
+    }
 
 }
