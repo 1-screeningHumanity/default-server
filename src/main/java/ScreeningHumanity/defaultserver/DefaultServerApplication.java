ba51927974fee8b151691860678a634f520e91f8
KangBaekGwa <ksu9801@gmail.com>
2024-05-02 20:27:17 +0900

    chore : todo list 작성

--- src/main/java/ScreeningHumanity/defaultserver/DefaultServerApplication.java
+++ src/main/java/ScreeningHumanity/defaultserver/DefaultServerApplication.java
@@ -18,6 +18,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 //        }
 //)
 public class DefaultServerApplication {
+    //todo : 유레카 서버 등록되는거 확인 필요. + healthCheck 확인;
+    //todo : 공통 API 응답 값 방식 지정 필요.
+    //todo : Exception Handler 처리 필요. Validation 관련 처리 사전에 필요.
 
     public static void main(String[] args) {
         SpringApplication.run(DefaultServerApplication.class, args);
